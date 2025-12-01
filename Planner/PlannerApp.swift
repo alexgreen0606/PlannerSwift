5f2be76e87b27ae17b8b8a3b80c875e1c410e47d
Alex Green <alexgreen0606@gmail.com>
2025-12-01 10:36:40 -0600

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Alex Green on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
